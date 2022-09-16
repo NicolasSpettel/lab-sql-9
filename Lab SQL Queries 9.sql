42b04c5bfed382855ec4137588fdf7c29d63bc92
Nicolas Spettel <nicolas.spettel1991@gmail.com>
2022-09-16 17:40:18 +0200

    Lab SQL Queries 9 format

--- Lab SQL Queries 9.sql
+++ Lab SQL Queries 9.sql
@@ -1,27 +1,27 @@
 -- Lab | SQL Queries 9
 
 -- In this lab we will find the customers who were active in consecutive months of May and June. Follow the steps to complete the analysis.
-use sakila;
+USE sakila;
 
 -- Create a table rentals_may to store the data from rental table with information for the month of May.
 -- Insert values in the table rentals_may using the table rental, filtering values only for the month of May.
-drop table if exists rentals_may;
-create table rentals_may as
-select * from rental where rental_date like '____-05-%';
+DROP TABLE IF EXISTS rentals_may;
+CREATE TABLE rentals_may AS
+SELECT * FROM rental WHERE rental_date LIKE '____-05-%';
 
-select * from rentals_may;
+SELECT * FROM rentals_may;
 
 -- Create a table rentals_june to store the data from rental table with information for the month of June.
 -- Insert values in the table rentals_june using the table rental, filtering values only for the month of June.
-drop table if exists rentals_june;
-create table rentals_june as
-select * from rental where rental_date like '____-06-%';
+DROP TABLE IF EXISTS rentals_june;
+CREATE TABLE rentals_june AS
+SELECT * FROM rental WHERE rental_date LIKE '____-06-%';
 
-select * from rentals_june;
+SELECT * FROM rentals_june;
 -- Check the number of rentals for each customer for May.
-select customer_id, count(rental_id) as number_of_rentals from rentals_may group by customer_id;
+SELECT customer_id, COUNT(rental_id) AS number_of_rentals FROM rentals_may GROUP BY customer_id;
 
 -- Check the number of rentals for each customer for June.
-select customer_id, count(rental_id) as number_of_rentals from rentals_june group by customer_id;
+SELECT customer_id, COUNT(rental_id) AS number_of_rentals FROM rentals_june GROUP BY customer_id;
 
 -- Create a Python connection with SQL database and retrieve the results of the last two queries (also mentioned below) as dataframes:
